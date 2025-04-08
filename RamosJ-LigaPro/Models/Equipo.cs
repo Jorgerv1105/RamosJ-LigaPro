d50699fa1cc05e4dac8abdafd9dc14b3538c9eca
Adamas <Adamas@ADAMAS>
2025-04-07 21:06:55 -0500

    Agregar archivos de proyecto.

---
+++ RamosJ-LigaPro/Models/Equipo.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace RamosJ_LigaPro.Models
+{
+    public class Equipo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        [DisplayName("Nombre del equipo")]
+        public string Nombre { get; set; }
+        [Range(0, 100)]
+        public string PartidosJugados { get; set; }
+        [Range(0, 100)]
+        public string PartidosGanados { get; set; }
+        [Range(0, 100)]
+        public string PartidosEmpatados { get; set; }
+        [Range(0, 100)]
+        public string PartidosPerdidos { get; set; }
+    }
+}
