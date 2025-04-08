37eed504067a4cdac3698f6f2c1d70bee6d272fe
Jorge Ramos <jorge.ramos.vasquez@udla.edu.ec>
2025-04-08 01:25:24 -0500

    Orden por medio de centencias

--- RamosJ-LigaPro/Controllers/EquipoController.cs
+++ RamosJ-LigaPro/Controllers/EquipoController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using RamosJ_LigaPro.Models;
+using RamosJ_LigaPro.Repositories;
 
 namespace RamosJ_LigaPro.Controllers
 {
@@ -8,27 +9,11 @@ namespace RamosJ_LigaPro.Controllers
     {
         public ActionResult List()
         {
-            List<Equipo> equipos = new List<Equipo>();
-            Equipo equipo1 = new Equipo
-            {
-                Id = 1,
-                Nombre = "Barcelona SC",
-                PartidosJugados = 10,
-                PartidosGanados = 7,
-                PartidosEmpatados = 2,
-                PartidosPerdidos = 1
-            };
-            equipos.Add(equipo1);
-            Equipo equipo2 = new Equipo
-            {
-                Id = 2,
-                Nombre = "Emelec",
-                PartidosJugados = 10,
-                PartidosGanados = 5,
-                PartidosEmpatados = 3,
-                PartidosPerdidos = 2
-            };
-            equipos.Add(equipo2);
+            EquipoRepository repository = new EquipoRepository();
+            var equipos = repository.DevuelveListadoEquipos();
+
+            equipos = equipos.OrderBy(item => item.PartidosGanados);
+            equipos = equipos.Where(item => item.Nombre == "Liga de Quito");
             return View(equipos);
         }
     }
--- RamosJ-LigaPro/Repositories/EquipoRepository.cs
+++ RamosJ-LigaPro/Repositories/EquipoRepository.cs
@@ -1,6 +1,33 @@
-﻿namespace RamosJ_LigaPro.Repositories
+﻿using RamosJ_LigaPro.Models;
+
+namespace RamosJ_LigaPro.Repositories
 {
     public class EquipoRepository
     {
+        public IEnumerable<Equipo> DevuelveListadoEquipos()
+        {
+            List<Equipo> equipos = new List<Equipo>();
+            Equipo equipo1 = new Equipo
+            {
+                Id = 1,
+                Nombre = "Barcelona SC",
+                PartidosJugados = 10,
+                PartidosGanados = 7,
+                PartidosEmpatados = 2,
+                PartidosPerdidos = 1
+            };
+            equipos.Add(equipo1);
+            Equipo equipo2 = new Equipo
+            {
+                Id = 2,
+                Nombre = "Emelec",
+                PartidosJugados = 10,
+                PartidosGanados = 5,
+                PartidosEmpatados = 3,
+                PartidosPerdidos = 2
+            };
+            equipos.Add(equipo2);
+            return equipos;
+        }
     }
 }
