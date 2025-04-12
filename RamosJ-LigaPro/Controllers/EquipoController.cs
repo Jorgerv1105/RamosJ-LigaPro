49d83e4d380c3904e64816eba755cf7b8893e67e
Jorge Ramos <jorge.ramos.vasquez@udla.edu.ec>
2025-04-12 00:36:34 -0500

    Añadir gestión de equipos y mejorar vistas
    Se ha implementado un repositorio de equipos en el controlador `EquipoController`, eliminando la creación de instancias en el método `List` y añadiendo métodos para crear y editar equipos.
    
    En `EquipoRepository`, se ha añadido una propiedad `Equipos` y métodos para obtener y actualizar equipos.
    
    Se han mejorado los enlaces de acción en `List.cshtml` con clases de Bootstrap y se han creado nuevas vistas `Create.cshtml` y `Edit.cshtml` con formularios de validación para mejorar la experiencia del usuario.

--- RamosJ-LigaPro/Controllers/EquipoController.cs
+++ RamosJ-LigaPro/Controllers/EquipoController.cs
@@ -7,14 +7,48 @@ namespace RamosJ_LigaPro.Controllers
 {
     public class EquipoController : Controller
     {
+        EquipoRepository _repository;
+        public EquipoController()
+        {
+            _repository = new EquipoRepository();
+        }
         public ActionResult List()
         {
-            EquipoRepository repository = new EquipoRepository();
-            var equipos = repository.DevuelveListadoEquipos();
+ 
+            var equipos = _repository.DevuelveListadoEquipos();
 
             equipos = equipos.OrderBy(item => item.PartidosGanados);
-            equipos = equipos.Where(item => item.Nombre == "Barcelona SC");
+            //equipos = equipos.Where(item => item.Nombre == "Barcelona SC");
             return View(equipos);
+
         }
+        public ActionResult Create()
+
+        {
+            return View();
+        }
+
+        public ActionResult Edit(int Id)
+
+        {
+    
+            var equipo = _repository.DevuelveEquipoPorID(Id);
+            return View(equipo);
+        }
+        [HttpPost]
+        public ActionResult Edit(int Id, Equipo equipo)
+        {
+            try
+            {
+  
+                _repository.ActualizaEquipo(Id, equipo);
+                return RedirectToAction(nameof(List));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+           
     }
 }
--- RamosJ-LigaPro/Repositories/EquipoRepository.cs
+++ RamosJ-LigaPro/Repositories/EquipoRepository.cs
@@ -4,13 +4,18 @@ namespace RamosJ_LigaPro.Repositories
 {
     public class EquipoRepository
     {
+        public IEnumerable<Equipo> Equipos;
+        public EquipoRepository() 
+        {
+            Equipos = DevuelveListadoEquipos();
+        }
         public IEnumerable<Equipo> DevuelveListadoEquipos()
         {
             List<Equipo> equipos = new List<Equipo>();
             Equipo equipo1 = new Equipo
             {
                 Id = 1,
-                Nombre = "Barcelona SC",
+                Nombre = "Barcelona",
                 PartidosJugados = 10,
                 PartidosGanados = 7,
                 PartidosEmpatados = 2,
@@ -28,6 +33,16 @@ namespace RamosJ_LigaPro.Repositories
             };
             equipos.Add(equipo2);
             return equipos;
+        }
+        public Equipo DevuelveEquipoPorID(int Id)
+        {
+
+            var equipo = Equipos.First(item=> item.Id == Id);
+            return equipo;
+        }
+        public bool ActualizaEquipo(int Id, Equipo equipo)
+        {
+            return true;
         }
     }
 }
